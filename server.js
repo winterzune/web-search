3217e1d7e9df8c2c4ce90d8493e578c989239e1c
Theo Elnecave <theo@Theos-MacBook-Pro.local>
2019-02-09 18:06:09 -0500

    gggggg

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
+
 app.use(routes);
 
 // Connect to the Mongo DB
